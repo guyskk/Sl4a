4331f4097568b740147df8e2b58743adb6b2c0f9
Sergio Maeso <massesos@gmail.com>
2012-12-13 10:09:38 +0100

    Ascii Problems solved.
    Change in the code to avoid ascii encoding related problems. The
    function WriteToHtml has been removed.

--- sms_backup.py
+++ sms_backup.py
@@ -2,50 +2,50 @@ import android
 import datetime
 import os
 
-
-def writeToHtml(string):
-	fh = open( '/sdcard/Backup_sms.html' ,'a')
-	fh.write(string)
-	fh.close
-
 droid = android.Android()
 
+#Get The Messages
 sms_received = droid.smsGetMessages(False,'inbox').result
 sms_sent = droid.smsGetMessages(False,'sent').result
 #sms_list.extend(droid.smsGetMessages(False,'sent').result)
 
+#Remove Old Backup
 os.remove('/sdcard/Backup_sms.html')
+
+#Create and open new file
+fh = open( '/sdcard/Backup_sms.html' ,'a')
 
 print "SMS LIST:"
 
-writeToHtml("<html>\n<head>\n<title>SMS Backup</title>\n</head>\n<body>\n<table border=1><td><b> From: </b></td><td><b> To: </b></td><td><b> Date: </b></td><td><b> Text: </b></td><tr>\n")
+fh.write("<html>\n<head>\n<title>SMS Backup</title>\n</head>\n<body>\n<table border=1><td><b> From: </b></td><td><b> To: </b></td><td><b> Date: </b></td><td><b> Text: </b></td><tr>\n")
 
 for message in sms_received:
 	text = message['body']
+	text = text.encode('ascii','replace')
 	number = message['address']
 	date_raw = datetime.datetime.fromtimestamp(int(message['date'])/1000)
 	date = date_raw.strftime("%m/%d/%y %H:%M:%S")
 
 	print "From: "+ number +" at: "+ date
-	print "Text: "+ text
-	print "---"
+	print "  -> "+ text
 
 	outstr = "<td> "+ number +" </td><td> Me </td><td> "+ date +" </td><td> "+ text +" </td><tr>\n"
-	writeToHtml(outstr)
+	fh.write(outstr)
 
 for message in sms_sent:
-	text = message['body']
+	text = unicode(message['body'])
+	text = text.encode('ascii','replace')
 	number = message['address']
 	date_raw = datetime.datetime.fromtimestamp(int(message['date'])/1000)
 	date = date_raw.strftime("%m/%d/%y %H:%M:%S")
 
 	print "To: "+ number +" at: "+ date
-	print "Text: "+ text
-	print "---"
+	print "  -> "+ text
 
 	outstr = "<td> Me </td><td> "+ number +" </td><td> "+ date +" </td><td> "+ text +" </td><tr>\n"
-	writeToHtml(outstr)
+	fh.write(outstr)
 
-writeToHtml("</table>\n</body>")
+fh.write("</table>\n</body>")
+fh.close()
 
 droid.webViewShow('file:///sdcard/Backup_sms.html')
