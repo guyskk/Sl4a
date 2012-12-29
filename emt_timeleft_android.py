35a67722ffabec5b94d70ed9435f90cd0f95f116
Sergio Maeso <massesos@gmail.com>
2012-12-29 18:17:06 +0100

    emt_timeleft added
    Small script that shows the time remaining until the bus arrives at your
    stop

---
+++ emt_timeleft_android.py
@@ -0,0 +1,44 @@
+#import library to do http requests:
+import urllib2
+ 
+#import easy to use xml parser called minidom:
+from xml.dom.minidom import *
+
+import android
+droid = android.Android()
+
+passkey = '0810DDE4-02FC-4C0E-A440-1BD171B397C8'
+idClient = "WEB.PORTALMOVIL.OTROS"
+
+parada = str(droid.dialogGetInput('EMT Timeleft','Inserte Numero de Parada').result)
+
+linea_query = str(droid.dialogGetInput('EMT Timeleft','Inserte Numero de Linea').result)
+
+print("")
+
+#download the file:
+file = urllib2.urlopen('https://servicios.emtmadrid.es:8443/geo/servicegeo.asmx/getArriveStop?idClient='+idClient+'&passKey='+passkey+'&idStop='+parada+'&statistics=&cultureInfo=')
+#convert to string:
+data = file.read()
+#close file because we dont need it anymore:
+file.close()
+#parse the xml you downloaded
+dom = parseString(data)
+nodos = dom.childNodes
+lista = nodos[0].getElementsByTagName("Arrive")
+i = 0
+for nodo in lista:
+	linea = nodo.getElementsByTagName("idLine")[0].toxml().replace('<idLine>','').replace('</idLine>','')
+	destino = nodo.getElementsByTagName("Destination")[0].toxml().replace('<Destination>','').replace('</Destination>','')
+	tiempo = int(nodo.getElementsByTagName("TimeLeftBus")[0].toxml().replace('<TimeLeftBus>','').replace('</TimeLeftBus>',''))/60
+	if tiempo==16666:
+		tiempo = "mas de 20"
+	distancia = nodo.getElementsByTagName("DistanceBus")[0].toxml().replace('<DistanceBus>','').replace('</DistanceBus>','')
+
+	texto = "Bus de la Linea "+ str(linea) +" con destino "+ destino+ " a " +str(tiempo)+" minutos, "+str(distancia)+ " metros."
+
+	if int(linea_query) == int(linea) or int(linea_query)==0:
+		print "["+ str(i) +"] "+ texto
+		droid.ttsSpeak(texto)
+
+ 	i=i+1
